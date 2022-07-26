d34306ee4ff8b4812e8fcfeea20e581aae4c2c57
kn3sv1 <kn3sv1@example.com>
2022-07-26 13:39:19 +0300

    example1

---
+++ 01_basic/pages/api/hello.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default (_: NextApiRequest, res: NextApiResponse) => {
+   res.status(200).json({ text: 'Welcome to TutorialsPoint' })
+}
+
+
+// https://www.tutorialspoint.com/nextjs/nextjs_typescript.htm
--- 01_basic/pages/index.js
+++ 01_basic/pages/index.js
@@ -1,3 +1,4 @@
+import Router from 'next/router'
 import Link from 'next/link'
 import Head from 'next/head'
 
@@ -9,6 +10,10 @@ function HomePage(props) {
          </Head>
          <div>Welcome to Next.js!</div>
          <Link href="/posts/first"><a>First Post</a></Link>
+         <br/>
+         <span style={{cursor:"pointer",textDecoration:"underline",}} onClick={() => Router.push('/posts/one')}>First Post onClick = Router.push</span>
+         <br/>
+         <span onClick={() => Router.push('/?counter=1', undefined, { shallow: true })}>Reload</span>
          <br/>
          <div>Next stars: {props.stars}</div>
          <img src="/logo.png" alt="TutorialsPoint Logo" />
